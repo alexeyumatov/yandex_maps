5454dbf2dbd359ed540613df700e418617b78212
monik <Alex.uma2007@yandex.ru>
2023-02-06 16:44:29 +0500

    task #1

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from samples.mapapi_PG import show_map
+
+
 def main():
-    pass
+    latitude, lontitude = input(), input()
+    spn = input()
+    show_map(f"ll={latitude},{lontitude}&spn={spn}", "map")
 
 
 if __name__ == '__main__':
